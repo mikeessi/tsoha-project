e00f95890be65b30b87db90b38f06c0206cf5723
Anttonen Mikko <mikantto@cubbli22-gold.local>
2022-09-19 02:42:15 +0300

    Improve error messaging

--- routes.py
+++ routes.py
@@ -6,10 +6,22 @@ import users
 def index():
     return render_template("index.html")
 
-@app.route("/login", methods=["POST"])
+@app.route("/login", methods=["POST", "GET"])
 def login():
-    username = request.form["username"]
-    password = request.form["password"]
-    if users.login(username, password):
-        return "Tervetuloa"
-    return render_template("error.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return "Tervetuloa"
+    return render_template("error.html", page = "index", message="Väärä tunnus tai salasana")
+
+@app.route("/register", methods=["POST","GET"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        if len(username) < 5 or len(username) > 12:
+            return render_template("error.html", page = "register", message="Tunnuksen tulee olla 5-12 merkkiä pitkä.")
