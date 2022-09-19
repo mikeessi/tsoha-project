4f746f53d498033701956811aa38c1cfa16cdc21
Anttonen Mikko <mikantto@cubbli22-gold.local>
2022-09-19 14:37:19 +0300

    Update registering

--- routes.py
+++ routes.py
@@ -15,7 +15,7 @@ def login():
         password = request.form["password"]
         if users.login(username, password):
             return "Tervetuloa"
-    return render_template("error.html", page = "index", message="Väärä tunnus tai salasana")
+    return render_template("error.html", page="index", message="Väärä tunnus tai salasana")
 
 @app.route("/register", methods=["POST","GET"])
 def register():
@@ -24,4 +24,19 @@ def register():
     if request.method == "POST":
         username = request.form["username"]
         if len(username) < 5 or len(username) > 12:
-            return render_template("error.html", page = "register", message="Tunnuksen tulee olla 5-12 merkkiä pitkä.")
+            return render_template("error.html", page="register", message="Tunnuksen tulee olla 5-12 merkkiä pitkä.")
+        if not users.check_username(username):
+            return render_template("error.html", page="register", message="Käyttäjänimi varattu")
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", page="register", message="Salasanat eivät täsmää")
+        if password1 == "":
+            return render_template("error.html", page="register", message="Salasana ei saa olla tyhjä")
+        user_role = request.form["user_role"]
+        if user_role not in ["1", "2"]:
+            return render_template("error.html", page="register", message="Epäkelpo käyttärooli")
+        if not users.create_account(username, password1, user_role):
+            return render_template("error.html", page="register", message="Rekisteröinti epäonnistui")
+        return redirect("/")
+
