3665f97bfcb0f48b86d3db7597d32466423094d6
Anttonen Mikko <mikantto@cubbli22-gold.local>
2022-10-03 15:40:05 +0300

    Improve error messaging

--- routes.py
+++ routes.py
@@ -16,29 +16,32 @@ def login():
         password = request.form["password"]
         if users.login(username, password):
             return redirect("/")
-    return render_template("error.html", page="index", message="Väärä tunnus tai salasana")
+    return render_template("error.html", page="index", messages=["Väärä tunnus tai salasana"])
 
 @app.route("/register", methods=["POST","GET"])
 def register():
+    errors = []
     if request.method == "GET":
         return render_template("register.html")
     if request.method == "POST":
         username = request.form["username"]
         if len(username) < 5 or len(username) > 12:
-            return render_template("error.html", page="register", message="Tunnuksen tulee olla 5-12 merkkiä pitkä.")
+            errors.append("Tunnuksen täytyy olla 5-12 merkkiä pitkä")
         if not users.check_username(username):
-            return render_template("error.html", page="register", message="Käyttäjänimi varattu")
+            errors.append("Käyttäjänimi varattu")
         password1 = request.form["password1"]
         password2 = request.form["password2"]
         if password1 != password2:
-            return render_template("error.html", page="register", message="Salasanat eivät täsmää")
+            errors.append("Salasanat eivät täsmää")
         if password1 == "":
-            return render_template("error.html", page="register", message="Salasana ei saa olla tyhjä")
+            errors.append("Salasana ei saa olla tyhjä")
         user_role = request.form["user_role"]
         if user_role not in ["1", "2"]:
-            return render_template("error.html", page="register", message="Epäkelpo käyttärooli")
+            errors.append("Epäkelpo käyttäjärooli")
+        if len(errors) > 0:
+            return render_template("error.html", page="register", messages=errors)
         if not users.create_account(username, password1, user_role):
-            return render_template("error.html", page="register", message="Rekisteröinti epäonnistui")
+            return render_template("error.html", page="register", messages="Rekisteröinti epäonnistui")
         return redirect("/")
 
 @app.route("/logout")
@@ -57,20 +60,23 @@ def add_gym():
     if request.method == "GET":
         return render_template("add_gym.html")
     if request.method == "POST":
+        errors = []
         users.check_csrf_token(request.form["csrf_token"])
         gym_name = request.form["gym_name"]
         address = request.form["address"]
         creator_id = users.get_user_id()
         if not gyms.check_gym_name(gym_name):
-            return render_template("error.html", page="add_gym", message="Salin nimi varattu")
+            errors.append("Salin nimi varattu")
         if len(gym_name) < 2 or len(gym_name) > 30:
-            return render_template("error.html", page="add_gym", message="Nimen täytyy olla 2-30 merkkiä pitkä")
+            errors.append("Nimen täytyy olla 2-30 merkkiä pitkä")
         if len(address) > 30:
-            return render_template("error.html", page="add_gym", message="Osoite saa olla korkeintaan 30 merkkiä pitkä")
+            errors.append("Osoite saa olla korkeintaa 30 merkkiä pitkä")
         if not address:
-            return render_template("error.hetml", page="add_gym", message="Lisää salin osoite")
+            errors.append("Lisää salin osoite")
+        if len(errors) > 0:
+            return render_template("error.html", page="add_gym", messages=errors)
         if not gyms.add_new_gym(gym_name, address, creator_id):
-            return render_template("error.html", page="add_gym", message="Salin lisäys epäonnistui")
+            return render_template("error.html", page="add_gym", messages="Salin lisäys epäonnistui")
     return redirect("/gyms")
 
 @app.route("/gyms/<int:gym_id>")
@@ -84,15 +90,18 @@ def add_wall(gym_id):
     if request.method == "GET":
         return render_template("add_wall.html", gym_id=gym_id)
     if request.method == "POST":
+        errors = []
         users.check_csrf_token(request.form["csrf_token"])
         wall_name = request.form["wall_name"]
         wall_description = request.form["wall_description"]
         if gyms.check_wall(gym_id, wall_name):
-            return render_template("error.html", page="add_wall", message="Seinä on jo olemassa")
+            errors.append("Seinä on jo olemassa")
         if len(wall_name) < 1 or len(wall_name) > 20:
-            return render_template("error.html", page="add_wall", message="Seinän nimi täytyy olla 1-20 merkkiä pitkä")
+            errors.append("Seinän nimi täytyy olla 1-20 merkkiä pitkä")
         if len(wall_description) < 1 or len(wall_description) > 20:
-            return render_template("error.html", page="add_wall", message="Seinän kuvauksen täytyy olla 1-20 merkkiä pitkä")
+            errors.append("Seinän kuvauksen täytyy olla 1-20 merkkiä pitkä")
+        if len(errors) > 0:
+            return render_template("error.html", page="add_wall", messages=errors)
         if not gyms.add_new_wall(wall_name, wall_description, gym_id):
-            return render_template("error.html", page="add_wall", message="Seinän lisäys epäonnistui")
+            return render_template("error.html", page="add_wall", messages="Seinän lisäys epäonnistui")
     return redirect(f"/gyms/{gym_id}")
