1bd4634d594e6b41b387d8d4d50c7c5e2ddea81e
Anttonen Mikko <mikantto@cubbli22-gold.local>
2022-10-05 13:46:54 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,23 @@
+COLORS = {"green":"Vihreä",
+          "neon_green":"Neonvihreä",
+          "yellow":"Keltainen",
+          "black":"Musta",
+          "white":"Valkoinen",
+          "grey":"Harmaa",
+          "red":"Punainen",
+          "blue":"Sininen",
+          "purple":"Purppura",
+          "pink":"Pinkki",
+          "turquoise":"Turkoosi"
+         }
+
+GRADES = {1:"4",
+          2:"5",
+          3:"6a",
+          4:"6b",
+          5:"6c",
+          6:"7a",
+          7:"7b",
+          8:"7c",
+          9:"8a"
+         }
